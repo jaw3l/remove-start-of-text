e798962ac83b2fa613fec3f4ab972cd3ed099b03
jaw3l <16852455+jaw3l@users.noreply.github.com>
2023-11-02 16:14:29 +0300

    using stx instead of sot
    - Changed class name to `RemoveSTX` instead of `RemoveUnicode`
    - Shortened notification contents (16&21)
    - Changed status bar element's text to `STX` instead of `SoT`
    - Updated `README.md`

--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
 
-export default class RemoveUnicode extends Plugin {
+export default class RemoveSTX extends Plugin {
 
 	async onload() {
 
 		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('x-circle', 'Remove Start-of-Text', (evt: MouseEvent) => {
+		this.addRibbonIcon('x-circle', 'Remove Start-of-Text (STX)', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 			if (markdownView) {
@@ -13,25 +13,25 @@ export default class RemoveUnicode extends Plugin {
 				const wholeText = editor.getValue();
 				const count = (wholeText.match(/\u0002/g) || []).length;
 				if (count === 0) {
-					new Notice('No start-of-text characters found.');
+					new Notice('No STX characters found.');
 					return;
 				} else if (count !== 0) {
 					const newText = wholeText.replace(/\u0002/g, '');
 					editor.setValue(newText);
-					new Notice('Removed ' + count + ' start-of-text characters.');
+					new Notice('Removed ' + count + ' STX characters.');
 				};
 			};
 		});
 
 		// This adds a status bar item.
-		const statusBarEl = this.addStatusBarItem();
-		statusBarEl.setText("SoT");
+		const statusBarElement = this.addStatusBarItem();
+		statusBarElement.setText("STX");
 		// when editor refreshes, remove the start-of-text character
 		this.registerEvent(this.app.workspace.on('editor-change', (editor: Editor) => {
 			const wholeText = editor.getValue();
 			const count = (wholeText.match(/\u0002/g) || []).length;
 			// Update the status bar item
-			statusBarEl.setText(count.toString());
+			statusBarElement.setText(count.toString());
 		}));
 
 		// This adds an editor command that can perform some operation on the current editor instance
