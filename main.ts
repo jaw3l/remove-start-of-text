a8d49b81c9866f6b69946f1d1e574d718dd1623d
jaw3l <16852455+jaw3l@users.noreply.github.com>
2023-11-02 21:30:10 +0300

    code improvement
    - Removed unnecessary blank lines
    - Removed comments as it is clear (enough) to understand code itself
    - Added `updateStatusBar` function to reduce redundancy

--- main.ts
+++ main.ts
@@ -1,52 +1,106 @@
-import { Editor, MarkdownView, Notice, Plugin } from 'obsidian';
+import { App, Editor, MarkdownView, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+
+interface RemoveSTXSettings {
+	hideIfNoSTX: boolean;
+}
+
+const DEFAULT_SETTINGS: RemoveSTXSettings = {
+	hideIfNoSTX: true,
+}
 
 export default class RemoveSTX extends Plugin {
+	settings: RemoveSTXSettings;
+	stxChar = new RegExp("\x02", "g");
 
 	async onload() {
+		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
 		this.addRibbonIcon('x-circle', 'Remove Start-of-Text (STX)', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
 			const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
 			if (markdownView) {
 				const editor = markdownView.editor;
 				const wholeText = editor.getValue();
-				const count = (wholeText.match(/\u0002/g) || []).length;
-				if (count === 0) {
+				const stxCount = (wholeText.match(this.stxChar) || []).length;
+				if (stxCount === 0) {
 					new Notice('No STX characters found.');
-					return;
-				} else if (count !== 0) {
-					const newText = wholeText.replace(/\u0002/g, '');
+				} else {
+					const newText = wholeText.replace(this.stxChar, '');
 					editor.setValue(newText);
-					new Notice('Removed ' + count + ' STX characters.');
-				};
-			};
+					new Notice(`Removed ${stxCount} STX characters.`);
+				}
+			} else {
+				new Notice('No active markdown editor.');
+			}
 		});
 
-		// This adds a status bar item.
 		const statusBarElement = this.addStatusBarItem();
-		statusBarElement.setText("STX");
-		// when editor refreshes, remove the start-of-text character
+		this.updateStatusBar(statusBarElement);
+
 		this.registerEvent(this.app.workspace.on('editor-change', (editor: Editor) => {
-			const wholeText = editor.getValue();
-			const count = (wholeText.match(/\u0002/g) || []).length;
-			// Update the status bar item
-			statusBarElement.setText(count.toString());
+			this.updateStatusBar(statusBarElement);
 		}));
 
-		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'remove-start-of-text-editor',
 			name: 'Remove start-of-text character',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
+			editorCallback: (editor: Editor) => {
 				const wholeText = editor.getValue();
-				const newText = wholeText.replace(/\u0002/g, '');
+				const newText = wholeText.replace(this.stxChar, '');
 				editor.setValue(newText);
 			}
 		});
+
+		this.addSettingTab(new RemoveSTXSettings(this.app, this));
+	}
+
+	updateStatusBar(statusBarElement: HTMLElement) {
+		// TODO: This only updates when the editor changes.
+		// TODO: Problem is that the editor doesn't change when Obsidian opens a new file.
+		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (markdownView) {
+			const wholeText = markdownView.editor.getValue();
+			const stxCount = (wholeText.match(this.stxChar) || []).length;
+			if (stxCount === 0 && this.settings.hideIfNoSTX) {
+				statusBarElement.setText('');
+			} else {
+				statusBarElement.setText(`${stxCount.toString()} STX`);
+			}
+		} // TODO: else?
 	}
 
 	onunload() {
+	}
 
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+}
+
+class RemoveSTXSettings extends PluginSettingTab {
+	plugin: RemoveSTX;
+
+	constructor(app: App, plugin: RemoveSTX) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName('Hide If No STX')
+			.setDesc('Hide the status bar item if there are no STX characters in the current file.')
+			.addToggle((toggle) => {
+				toggle.setValue(this.plugin.settings.hideIfNoSTX);
+				toggle.onChange(async (value) => {
+					this.plugin.settings.hideIfNoSTX = value;
+					await this.plugin.saveSettings();
+				});
+			});
 	}
 }
